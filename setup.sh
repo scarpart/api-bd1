aff71002d88f8b4941181ca663af047306c21755
scarpart <aenartzr@gmail.com>
2023-11-19 01:28:29 -0300

    update: setup.sh now accounts for DML script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 
-echo "Começando o setup... se der errado, provavelmente tens que instalar um postgres16-alpine ou algo assim como imagem do docker."
+echo "Começando o setup... se der errado, provavelmente tens que instalar um postgres:16-alpine ou algo assim como imagem do docker."
 docker-compose up -d 
 make createdb 
 make setpass
 make migrateup 
+make dml-script
 echo "...Setup completo! Banco de Dados criado, scripts DDL e DML executados, e PostgreSQL rodando na porta 7654 na máquina hospedeira."
